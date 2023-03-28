c880056feb77718f0e3402db452c0c91e45389ff
Joseph Llamas <joseph.llamas010@gmail.com>
2023-03-28 16:12:48 -0400

    remove node modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { UserContext } from "./context/UserContext";
-// import { useContext } from "react";
+import React from "react";
 import Homepage from "./components/Homepage";
 
 const App = () => {
-  // const { currentUser, setCurrentUser } = useContext(UserContext);
+  
   return (
     <>
       <BrowserRouter>
--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider 
-  domain={dev-b7gbnlo0ae4ymnns.us.auth0.com}
-  clientId={fxYaY2TLJJuCUaJV69Pre9s4FQtF8dA0}
+  domain="{dev-b7gbnlo0ae4ymnns.us.auth0.com}"
+  clientId="{fxYaY2TLJJuCUaJV69Pre9s4FQtF8dA0}"
   redirectUri={window.location.origin}
 >
   <React.StrictMode>
