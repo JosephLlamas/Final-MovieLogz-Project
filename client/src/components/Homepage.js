4b1dc3bcf2c9b20b4c577ef39cbf8e4f6abb70f9
Joseph Llamas <joseph.llamas010@gmail.com>
2023-03-28 14:31:40 -0400

    Joseph Llamas

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,2 +1,38 @@
+import { useState, useEffect } from "react";
 
+const Homepage = () => {
+  const [data, setData] = useState([])
 
+  useEffect(() => {
+    
+    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=79dc84aa2584757f2f9c4044429e028b&language=en-US&page=1")
+      .then(response => response.json())
+      .then((data) => {
+        setData(data.results);
+        console.log(data.results)
+      })
+      .catch(err => console.error(err));
+  }, [])
+
+    return (
+      <div>
+        {!data ? (
+          <p>loading</p>
+        ) : (
+        <>
+        {data.map((items) => {
+          return (
+            <div key={items.id}>
+              <p>{items.title}</p>
+              
+              <img src={`https://image.tmdb.org/t/p/w500/${items.backdrop_path}`} alt={items.title} />
+            </div>
+          )
+        })}
+        </>
+        )}
+      </div>
+    )
+}
+
+export default Homepage; 
